89671255f513626683137ba760cf2a5efee64b02
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-02-25 21:55:38 +0200

    Finish parser

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -1,14 +1,14 @@
 package com.craftinginterpreters.lox;
 
 class AstPrinter implements Expr.Visitor<String> {
+
   String print(Expr expr) {
     return expr.accept(this);
   }
 
   @Override
   public String visitBinaryExpr(Expr.Binary expr) {
-    return parenthesize(expr.operator.lexeme,
-                        expr.left, expr.right);
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
   }
 
   @Override
@@ -39,16 +39,15 @@ class AstPrinter implements Expr.Visitor<String> {
 
     return builder.toString();
   }
+  //   public static void main(String[] args) {
+  //     Expr expression = new Expr.Binary(
+  //         new Expr.Unary(
+  //             new Token(TokenType.MINUS, "-", null, 1),
+  //             new Expr.Literal(123)),
+  //         new Token(TokenType.STAR, "*", null, 1),
+  //         new Expr.Grouping(
+  //             new Expr.Literal(45.67)));
 
-//   public static void main(String[] args) {
-//     Expr expression = new Expr.Binary(
-//         new Expr.Unary(
-//             new Token(TokenType.MINUS, "-", null, 1),
-//             new Expr.Literal(123)),
-//         new Token(TokenType.STAR, "*", null, 1),
-//         new Expr.Grouping(
-//             new Expr.Literal(45.67)));
-
-//     System.out.println(new AstPrinter().print(expression));
-//   }
+  //     System.out.println(new AstPrinter().print(expression));
+  //   }
 }
--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -1,170 +1,171 @@
 package com.craftinginterpreters.lox;
 
-import java.util.List;
-
 import static com.craftinginterpreters.lox.TokenType.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 class Parser {
-	private static class ParseError extends RuntimeException {}
 
-	private final List<Token> tokens;
-	private int current = 0;
+  private static class ParseError extends RuntimeException {
 
-	Parser(List<Token> tokens) {
-		this.tokens = tokens;
-	}
+    static final long serialVersionUID = 1234567890L;
+  }
 
-	Expr parse() {
-		try {
-			return expression();
-		} catch (ParseError error) {
-			return null;
-		}
-	}
+  private final List<Token> tokens;
+  private int current = 0;
 
-	private Expr expression() {
-		return equality();
-	}
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
 
-	private Expr equality() {
-		Expr expr = comparison();
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
 
-		while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-			Token operator = previous();
-			Expr right = comparison();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+  private Expr expression() {
+    return equality();
+  }
 
-		return expr;
-	}
+  private Expr equality() {
+    Expr expr = comparison();
 
-	private Expr comparison() {
-		Expr expr = term();
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
-			Token operator = previous();
-			Expr right = term();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    return expr;
+  }
 
-		return expr;
-	}
+  private Expr comparison() {
+    Expr expr = term();
 
-	private Expr term() {
-		Expr expr = factor();
+    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		while (match(MINUS, PLUS)) {
-			Token operator = previous();
-			Expr right = factor();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    return expr;
+  }
 
-		return expr;
-	}
+  private Expr term() {
+    Expr expr = factor();
 
-	private Expr factor() {
-		Expr expr = unary();
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		while (match(SLASH, STAR)) {
-			Token operator = previous();
-			Expr right = unary();
-			expr = new Expr.Binary(expr, operator, right);
-		}
+    return expr;
+  }
 
-		return expr;
-	}
+  private Expr factor() {
+    Expr expr = unary();
 
-	private Expr unary() {
-		if (match(BANG, MINUS)) {
-		Token operator = previous();
-		Expr right = unary();
-			return new Expr.Unary(operator, right);
-		}
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
 
-		return primary();
-	}
+    return expr;
+  }
 
-	private Expr primary() {
-		if (match(FALSE)) return new Expr.Literal(false);
-		if (match(TRUE)) return new Expr.Literal(true);
-		if (match(NIL)) return new Expr.Literal(null);
+  private Expr unary() {
+    if (match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
 
-		if (match(NUMBER, STRING)) {
-			return new Expr.Literal(previous().literal);
-		}
+    return primary();
+  }
 
-		if (match(LEFT_PAREN)) {
-			Expr expr = expression();
-			consume(RIGHT_PAREN, "Expect ')' after expression.");
-			return new Expr.Grouping(expr);
-		}
+  private Expr primary() {
+    if (match(FALSE)) return new Expr.Literal(false);
+    if (match(TRUE)) return new Expr.Literal(true);
+    if (match(NIL)) return new Expr.Literal(null);
 
-		throw error(peek(), "Expect expression.");
-	}
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
 
-	private boolean match(TokenType... types) {
-		for (TokenType type : types) {
-			if (check(type)) {
-			  advance();
-			  return true;
-			}
-		}
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
 
-		return false;
-	}
+    throw error(peek(), "Expect expression.");
+  }
 
-	private Token consume(TokenType type, String message) {
-		if (check(type)) return advance();
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
 
-		throw error(peek(), message);
-	}
+    return false;
+  }
 
-	private boolean check(TokenType type) {
-		if (isAtEnd()) return false;
-		return peek().type == type;
-	}
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
 
-	private Token advance() {
-		if (!isAtEnd()) current++;
-		return previous();
-	}
+    throw error(peek(), message);
+  }
 
-	private boolean isAtEnd() {
-		return peek().type == EOF;
-	}
+  private boolean check(TokenType type) {
+    if (isAtEnd()) return false;
+    return peek().type == type;
+  }
 
-	private Token peek() {
-		return tokens.get(current);
-	}
+  private Token advance() {
+    if (!isAtEnd()) current++;
+    return previous();
+  }
 
-	private Token previous() {
-		return tokens.get(current - 1);
-	}
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
 
-	private ParseError error(Token token, String message) {
-		Lox.error(token, message);
-		return new ParseError();
-	}
+  private Token peek() {
+    return tokens.get(current);
+  }
 
-	private void synchronize() {
-		advance();
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
 
-		while (!isAtEnd()) {
-			if (previous().type == SEMICOLON) return;
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
 
-			switch (peek().type) {
-				case CLASS:
-				case FUN:
-				case VAR:
-				case FOR:
-				case IF:
-				case WHILE:
-				case PRINT:
-				case RETURN:
-					return;
-			}
+  private void synchronize() {
+    advance();
 
-			advance();
-		}
-	}
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      if (
+        Arrays
+          .asList(CLASS, FUN, VAR, FOR, IF, WHILE, PRINT, RETURN)
+          .contains(peek().type)
+      ) {
+        return;
+      }
+
+      advance();
+    }
+  }
 }
