df56c57fc72600d2882331f2b0667cd079c624a9
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-03-02 21:40:19 +0200

    Wire up interpreter

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,66 +9,77 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-	static boolean hadError = false;
-	public static void main(String[] args) throws IOException {
-		if (args.length > 1) {
-			System.out.println("Usage: jlox [script]");
-			System.exit(64);
-		} else if (args.length == 1) {
-			runFile(args[0]);
-		} else {
-			runPrompt();
-		}
-	}
 
-	private static void runFile(String path) throws IOException {
-		byte[] bytes = Files.readAllBytes(Paths.get(path));
-		run(new String(bytes, Charset.defaultCharset()));
+  private static final Interpreter interpreter = new Interpreter();
+  static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
-		// Indicate an error in the exit code.
-		if (hadError) System.exit(65);
-	}
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
 
-	private static void runPrompt() throws IOException {
-		InputStreamReader input = new InputStreamReader(System.in);
-		BufferedReader reader = new BufferedReader(input);
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
 
-		for (;;) {
-			System.out.print("> ");
-			String line = reader.readLine();
-			if (line == null) break;
-			run(line);
-			hadError = false;
-		}
-	}
+    // Indicate an error in the exit code.
+    if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
+  }
 
-	private static void run(String source) {
-		Scanner scanner = new Scanner(source);
-		List<Token> tokens = scanner.scanTokens();
-		Parser parser = new Parser(tokens);
-		Expr expression = parser.parse();
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
 
-		// Stop if there was a syntax error.
-		if (hadError) return;
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+  }
 
-		System.out.println(new AstPrinter().print(expression));
-	}
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-	static void error(int line, String message) {
-		report(line, "", message);
-	}
+    // Stop if there was a syntax error.
+    if (hadError) return;
 
-	private static void report(int line, String where, String message) {
-		System.err.println(
-			  "[line " + line + "] Error" + where + ": " + message);
-		hadError = true;
-	}
+    interpreter.interpret(expression);
+  }
 
-	static void error(Token token, String message) {
-		if (token.type == TokenType.EOF) {
-			report(token.line, " at end", message);
-		} else {
-			report(token.line, " at '" + token.lexeme + "'", message);
-		}
-	}
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(
+      error.getMessage() + "\n[line " + error.token.line + "]"
+    );
+    hadRuntimeError = true;
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
 }
