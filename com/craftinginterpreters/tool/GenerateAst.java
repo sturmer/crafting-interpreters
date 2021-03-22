c0980ff0e51510275ccc9199546815a91092a469
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-03-22 21:19:11 +0200

    Until 8.3 end

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -20,7 +20,8 @@ public class GenerateAst {
         "Binary   : Expr left, Token operator, Expr right",
         "Grouping : Expr expression",
         "Literal  : Object value",
-        "Unary    : Token operator, Expr right"
+        "Unary    : Token operator, Expr right",
+        "Variable : Token name"
       )
     );
 
@@ -29,7 +30,8 @@ public class GenerateAst {
       "Stmt",
       Arrays.asList(
         "Expression : Expr expression",
-        "Print      : Expr expression"
+        "Print      : Expr expression",
+        "Var        : Token name, Expr initializer"
       )
     );
   }
